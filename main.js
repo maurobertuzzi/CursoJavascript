941856a8c1092a45b6d6ad69828b1ec533423316
maurobertuzzi <mauro_bertuzzi@hotmail.com>
2023-04-24 00:52:05 -0300

    promedio de pesos

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-let numeroUno = parseFloat(prompt("Ingrese el primer numero"));
-let numeroDos = parseFloat(prompt("Ingrese el segundo numero"));
 
-alert(numeroUno + numeroDos)
+for (let turno = 1; turno <= 3; turno++) {
+  let nombre = prompt("Buena semana! por favor ingresa Tu nombre completo");
+  let piernas = parseInt(prompt("Por favor ingresa el peso que usas en piernas"));
+  let pecho = parseInt(prompt("Por favor ingresa el peso que usas en pecho"));
+  let espalda = parseInt(prompt("Por favor ingresa el peso que usas en espalda"));
+  let brazos = parseInt(prompt("Por favor ingresa el peso que usas en brazos y hombros"));
+  let peso = (piernas + pecho + espalda + brazos) / 4
+  if (peso < 15) {
+    alert(`${nombre}, Necesitas en promedio un peso de ${peso} Por favor ubicate en los Box del 1 al 6`);
+  } else {
+    alert(`${nombre}, Necesitas en promedio un peso de ${peso} Por favor ubicate en los Box del 7 al 13`);
+  }
+}
