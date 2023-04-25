01c12b18c84d25c4b516390994f533bbc4d64c94
maurobertuzzi <mauro_bertuzzi@hotmail.com>
2023-04-25 00:21:39 -0300

    agregue funcion para sacar promedio

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+function promedio(piernas, pecho, espalda, brazos){
+  return (piernas + pecho + espalda + brazos)/4
+}
 
 for (let turno = 1; turno <= 3; turno++) {
   let nombre = prompt("Buena semana! por favor ingresa Tu nombre completo");
@@ -5,7 +8,7 @@ for (let turno = 1; turno <= 3; turno++) {
   let pecho = parseInt(prompt("Por favor ingresa el peso que usas en pecho"));
   let espalda = parseInt(prompt("Por favor ingresa el peso que usas en espalda"));
   let brazos = parseInt(prompt("Por favor ingresa el peso que usas en brazos y hombros"));
-  let peso = (piernas + pecho + espalda + brazos) / 4
+  let peso = promedio(piernas, pecho, espalda, brazos) 
   if (peso < 15) {
     alert(`${nombre}, Necesitas en promedio un peso de ${peso} Por favor ubicate en los Box del 1 al 6`);
   } else {
