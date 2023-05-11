321714f36f172eb7b04dd0a07e0a1b5fd6fb9fb1
maurobertuzzi <mauro_bertuzzi@hotmail.com>
2023-05-10 23:44:32 -0300

    Arreglo con filter y foreach

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
+const productos = [
+  { id: 1, nombreProd: "Remera", talle: "Medium", Precio: 1000 },
+  { id: 2, nombreProd: "Remera", talle: "Large", Precio: 1000 },
+  { id: 3, nombreProd: "Remera", talle: "Small", Precio: 1000 },
+  { id: 4, nombreProd: "Pantalon", talle: "Medium", Precio: 800 },
+  { id: 5, nombreProd: "Pantalon", talle: "Large", Precio: 800 },
+  { id: 6, nombreProd: "Calza Corta", talle: "Medium", Precio: 700 },
+  { id: 7, nombreProd: "Calza Corta", talle: "Small", Precio: 700 },
+  { id: 8, nombreProd: "Calza Larga", talle: "Medium", Precio: 800 },
+  { id: 9, nombreProd: "Calza Larga", talle: "Large", Precio: 800 },
+  { id: 10, nombreProd: "Buzo", talle: "Medium", Precio: 1200 },
+  { id: 11, nombreProd: "Buzo", talle: "Large", Precio: 1200 },
+  { id: 12, nombreProd: "Campera", talle: "Medium", Precio: 1500 },
+  { id: 13, nombreProd: "Campera", talle: "Large", Precio: 1500 },
+];
+
+let busqueda = prompt("Ingrese el nombre del producto");
+const encuentro = productos.filter(item => item.nombreProd.includes(busqueda));
+
+
+encuentro.forEach(item => {
+  if (encuentro) {
+    let mensaje = `
+  Producto: ${item.nombreProd}
+  Talle: ${item.talle}
+  Precio: ${item.Precio} 
+  `;
+    alert(mensaje);
+  } else {
+    alert("lamentablemente No tenemos ese producto")
+  }
+
+});
+
+/*
+if (encuentro) {
+  let mensaje = `
+  nombreProd: ${encuentro.nombreProd}
+  talle: ${encuentro.talle}
+  precio: ${encuentro.Precio} 
+  `;
+
+  alert(mensaje);
+} else {
+  alert("lamentablemente No tenemos ese producto")
+}
+*/
+
+
+/* -- PRIMER ENTREGA -- 
 function promedio(piernas, pecho, espalda, brazos){
   return (piernas + pecho + espalda + brazos)/4
 }
@@ -15,3 +65,4 @@ for (let turno = 1; turno <= 3; turno++) {
     alert(`${nombre}, Necesitas en promedio un peso de ${peso} Por favor ubicate en los Box del 7 al 13`);
   }
 }
+*/
