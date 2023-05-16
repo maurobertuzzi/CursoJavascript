9f2c0f8882cbff10aba6cbf11b8a0ae964774a23
maurobertuzzi <mauro_bertuzzi@hotmail.com>
2023-05-15 22:19:21 -0300

    entrega 2

--- main.js
+++ main.js
@@ -1,51 +1,35 @@
-const productos = [
-  { id: 1, nombreProd: "Remera", talle: "Medium", Precio: 1000 },
-  { id: 2, nombreProd: "Remera", talle: "Large", Precio: 1000 },
-  { id: 3, nombreProd: "Remera", talle: "Small", Precio: 1000 },
-  { id: 4, nombreProd: "Pantalon", talle: "Medium", Precio: 800 },
-  { id: 5, nombreProd: "Pantalon", talle: "Large", Precio: 800 },
-  { id: 6, nombreProd: "Calza Corta", talle: "Medium", Precio: 700 },
-  { id: 7, nombreProd: "Calza Corta", talle: "Small", Precio: 700 },
-  { id: 8, nombreProd: "Calza Larga", talle: "Medium", Precio: 800 },
-  { id: 9, nombreProd: "Calza Larga", talle: "Large", Precio: 800 },
-  { id: 10, nombreProd: "Buzo", talle: "Medium", Precio: 1200 },
-  { id: 11, nombreProd: "Buzo", talle: "Large", Precio: 1200 },
-  { id: 12, nombreProd: "Campera", talle: "Medium", Precio: 1500 },
-  { id: 13, nombreProd: "Campera", talle: "Large", Precio: 1500 },
+// SEGUNDA ENTREGA 
+ const productos = [
+  { id: 1, nombreProd: "remera", talle: "Medium", Precio: 1000 },
+  { id: 2, nombreProd: "remera", talle: "Large", Precio: 1000 },
+  { id: 3, nombreProd: "remera", talle: "Small", Precio: 1000 },
+  { id: 4, nombreProd: "pantalon", talle: "Medium", Precio: 800 },
+  { id: 5, nombreProd: "pantalon", talle: "Large", Precio: 800 },
+  { id: 6, nombreProd: "calza corta", talle: "Medium", Precio: 700 },
+  { id: 7, nombreProd: "calza corta", talle: "Small", Precio: 700 },
+  { id: 8, nombreProd: "calza larga", talle: "Medium", Precio: 800 },
+  { id: 9, nombreProd: "calza larga", talle: "Large", Precio: 800 },
+  { id: 10, nombreProd: "buzo", talle: "Medium", Precio: 1200 },
+  { id: 11, nombreProd: "buzo", talle: "Large", Precio: 1200 },
+  { id: 12, nombreProd: "campera", talle: "Medium", Precio: 1500 },
+  { id: 13, nombreProd: "campera", talle: "Large", Precio: 1500 },
 ];
 
 let busqueda = prompt("Ingrese el nombre del producto");
 const encuentro = productos.filter(item => item.nombreProd.includes(busqueda));
 
-
-encuentro.forEach(item => {
-  if (encuentro) {
+if (encuentro.length > 0) {
+  encuentro.forEach(item => {
     let mensaje = `
-  Producto: ${item.nombreProd}
-  Talle: ${item.talle}
-  Precio: ${item.Precio} 
-  `;
+      Producto: ${item.nombreProd}
+      Talle: ${item.talle}
+      Precio: ${item.Precio} 
+    `;
     alert(mensaje);
-  } else {
-    alert("lamentablemente No tenemos ese producto")
-  }
-
-});
-
-/*
-if (encuentro) {
-  let mensaje = `
-  nombreProd: ${encuentro.nombreProd}
-  talle: ${encuentro.talle}
-  precio: ${encuentro.Precio} 
-  `;
-
-  alert(mensaje);
+  });
 } else {
-  alert("lamentablemente No tenemos ese producto")
+  alert("Lamentablemente no tenemos ese producto");
 }
-*/
-
 
 /* -- PRIMER ENTREGA -- 
 function promedio(piernas, pecho, espalda, brazos){
